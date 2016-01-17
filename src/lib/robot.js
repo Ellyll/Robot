ea608ea639fcb18152c4472a1f62f2b589f59afe
Ellyll <cawsllyffant-git@yahoo.com>
2016-01-17 15:43:11 +0000

    Use composition and factories

--- src/lib/robot.js
+++ src/lib/robot.js
@@ -2,77 +2,51 @@
 
 import * as gfx from './gfx.js';
 
-class Component {
-    constructor(x,y) {
-        this._x = x;
-        this._y = y;
-        this._subComponents = [];
-    }
-    
-    addSubComponent(component) {
-        this._subComponents.push(component);
-    }
-    
-    get subComponents() {
-        return this._subComponents;
-    }
-    
-    get x() {
-        return this._x;
-    }
-    
-    get y() { return this._y; }
-}
 
-class Robot extends Component {
-    constructor(x, y, size) {
-        super(x, y);
-        this._size = size;
-        this.addSubComponent(new MainBody(x, y+size/4, size/4));
-    }
+const location = (x,y) => {
+    let _x = x;
+    let _y = y;
     
-    get size() { return this._size; }
-}
-
-class MainBody extends Component {
-    constructor(x, y, radius) {
-        super(x, y);
-        this._radius = radius;
-    }
-    
-    get radius() { return this._radius; }
-}
-
-class Head extends Component {
-    constructor(x, y, radius) {
-        super(x, y);
-        this._radius = radius;
-    }
-    
-    get radius() { return this._radius; }
-}
-
-
-function makeRobot(x, y, size) {
-    return new Robot(x,y, size);
-}
-
-function renderRobot(context, robot) {
-    console.log('renderRobot()', context, robot);
-    gfx.drawRectangle(context, robot.x-robot.size/2, robot.y-robot.size/2, robot.size, robot.size, 'green');
-    robot.subComponents.forEach( component => renderComponent(context, component) );
-}
-
-function renderComponent(context, component) {
-    console.log('renderComponent()', context, component);
-    if (component instanceof MainBody) {
-        renderMainBody(context, component);
+    return {
+        get x() { return _x; },
+        get y() { return _y; }
     };
-}
+};
 
-function renderMainBody(context, mainBody) {
+const head = (x, y, radius) => {
+    let _radius = radius;
+    return Object.assign({}, location(x,y), { get radius() { return _radius; } });
+};
+
+const mainBody = (x, y, radius) => {
+    let _radius = radius;
+    return Object.assign({}, location(x,y), { get radius() { return _radius; } });
+};
+
+const makeRobot = (x, y, size) => {
+    let _size = size;
+    let _mainBody = mainBody(x, y+size/4, size/4);
+
+    let robot = {
+        get size() { return _size; },
+        get mainBody() { return _mainBody; }
+    };
+    
+    return Object.assign(robot, location(x, y)); 
+};
+
+const renderMainBody = (context, mainBody) => {
     console.log('renderMainBody()', context, mainBody);
     gfx.drawCircle(context, mainBody.x, mainBody.y, mainBody.radius, 'white');
-}
+};
 
-export { makeRobot, renderRobot, renderComponent };
+const renderRobot = (context, robot) => {
+    console.log('renderRobot()', context, robot);
+    gfx.drawRectangle(context, robot.x-robot.size/2, robot.y-robot.size/2, robot.size, robot.size, 'green');
+
+    renderMainBody(context, robot.mainBody);
+};
+
+
+
+export { makeRobot, renderRobot };
