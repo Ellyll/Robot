8dc0b40bc2cb2a881efa5e3b8d736d608d49ccac
Ellyll <cawsllyffant-git@yahoo.com>
2016-01-18 13:27:05 +0000

    Movement

--- src/app.js
+++ src/app.js
@@ -14,7 +14,32 @@ function init() {
     let x = canvas.width / 2;
     let y = canvas.height / 2;
     let size = Math.min(canvas.width, canvas.height)/4;
+    
+    let speed = size/2000; // pixels / ms
+    let distanceToTravel = size*3;
      
     let robot1 = robot.makeRobot(x, y, size);
-    robot.renderRobot(context, robot1);
+    //robot.renderRobot(context, robot1);
+
+    window.requestAnimationFrame((currentTime) => {
+        draw(context, currentTime, currentTime, speed, distanceToTravel, robot1);
+    });    
+}
+
+function draw(context, currentTime, lastTime, speed, distanceToTravel, aRobot) {
+    let deltaTime = currentTime - lastTime;
+    let deltaDistance = speed * deltaTime;
+    
+    let newRobot = aRobot.moveRelative(deltaDistance);
+    gfx.clearCanvas(context);
+    robot.renderRobot(context, newRobot);
+    
+    let newLastTime = currentTime;
+    let newDistanceToTravel = distanceToTravel - deltaDistance;
+    
+    if (newDistanceToTravel <= 0) return;
+    
+    window.requestAnimationFrame((currentTime) => {
+        draw(context, currentTime, newLastTime, speed, distanceToTravel-deltaDistance, newRobot);
+    });    
 }
--- src/lib/robot.js
+++ src/lib/robot.js
@@ -2,8 +2,7 @@
 
 import * as gfx from './gfx.js';
 
-
-const location = (x,y) => {
+const hasLocation = (x,y) => {
     let _x = x;
     let _y = y;
     
@@ -13,40 +12,74 @@ const location = (x,y) => {
     };
 };
 
+const hasRadius = (radius) => {
+    let _radius = radius;
+    
+    return { get radius() { return _radius; } };
+};
+
+const hasAngle = (angle) => {
+    let _angle = angle;
+    
+    return { get angle() { return _angle; } };
+};
+
 const head = (x, y, radius) => {
-    let _radius = radius;
-    return Object.assign({}, location(x,y), { get radius() { return _radius; } });
+    return Object.assign({}, hasLocation(x,y), hasRadius(radius) );
 };
 
-const mainBody = (x, y, radius) => {
-    let _radius = radius;
-    return Object.assign({}, location(x,y), { get radius() { return _radius; } });
+const makeMainBody = (x, y, radius, angle) => {
+    let newMainBody;
+    let _moveRelative = (xDistance) => {
+        //console.log('_moveRelative', newMainBody);
+        let newAngle = newMainBody.angle + (xDistance/(2*Math.PI*newMainBody.radius))*Math.PI*2;
+        if (newAngle > 2*Math.PI) newAngle = newAngle % (2*Math.PI);
+        if (newAngle < -2*Math.PI) newAngle = newAngle % (-2*Math.PI);
+        if (newAngle < 0) newAngle = 2*Math.PI - newAngle;
+        return makeMainBody(newMainBody.x+xDistance, newMainBody.y, newMainBody.radius, newAngle);
+    };  
+    
+    newMainBody = Object.assign({}, hasLocation(x,y), hasRadius(radius), hasAngle(angle), { moveRelative: _moveRelative });
+    return newMainBody;
 };
 
-const makeRobot = (x, y, size) => {
+const makeRobot = (x, y, size, mainBody) => {
     let mainBodyRadius = size/3;
     let headRadius = mainBodyRadius*3/5;
     
     let _size = size;
-    let _mainBody = mainBody(x, y+mainBodyRadius/2, mainBodyRadius);
+    let _mainBody = mainBody || makeMainBody(x, y+mainBodyRadius/2, mainBodyRadius, 0);
     let _head = head(x, _mainBody.y-_mainBody.radius-headRadius/8, headRadius);
+    
+    let newRobot;
+    
+    let _moveRelative = (xDistance) => {
+        let newMainBody = _mainBody.moveRelative(xDistance);
+        return makeRobot(newRobot.x+xDistance, newRobot.y, newRobot.size, newMainBody);
+    };
 
-    let robot = {
+    newRobot = {
         get size() { return _size; },
         get mainBody() { return _mainBody; },
-        get head() { return _head; }
+        get head() { return _head; },
+        moveRelative: _moveRelative
     };
     
-    return Object.assign(robot, location(x, y)); 
+    newRobot = Object.assign(newRobot, hasLocation(x, y));
+    return newRobot; 
 };
 
 const renderMainBody = (context, mainBody) => {
-    console.log('renderMainBody()', context, mainBody);
+    //console.log('renderMainBody()', context, mainBody);
     gfx.drawCircle(context, mainBody.x, mainBody.y, mainBody.radius, 'white');
+    
+    let x2 = mainBody.x + mainBody.radius*Math.cos(mainBody.angle+Math.PI*3/2);
+    let y2 = mainBody.y + mainBody.radius*Math.sin(mainBody.angle+Math.PI*3/2);
+    gfx.drawLine(context, mainBody.x, mainBody.y, x2, y2, 'white');
 };
 
 const renderHead = (context, head) => {
-    console.log('renderHead()');
+    //console.log('renderHead()');
     gfx.drawArc(context, head.x, head.y, head.radius, Math.PI, 0, false, 'red');
     gfx.drawRectangle(context, head.x-head.radius, head.y, head.radius*2, head.radius/8, 'red');
     let points = [
@@ -75,8 +108,8 @@ const renderHead = (context, head) => {
 };
 
 const renderRobot = (context, robot) => {
-    console.log('renderRobot()', context, robot);
-    gfx.drawRectangle(context, robot.x-robot.size/2, robot.y-robot.size/2, robot.size, robot.size, 'green');
+    //console.log('renderRobot()', context, robot);
+    //gfx.drawRectangle(context, robot.x-robot.size/2, robot.y-robot.size/2, robot.size, robot.size, 'green');
 
     renderMainBody(context, robot.mainBody);
     renderHead(context, robot.head);
