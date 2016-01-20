32fc7efc04f595bba5ad96595c38df5cca184e2e
Ellyll <cawsllyffant-git@yahoo.com>
2016-01-20 15:38:18 +0000

    Off screen drawing

--- src/app.js
+++ src/app.js
@@ -10,6 +10,12 @@ function init() {
     let canvas = document.getElementById('canvas');
     let context = canvas.getContext('2d');
     gfx.maximiseCanvas(canvas);
+    
+    // createBuffer
+    let canvas2 = document.createElement('canvas');
+    canvas2.width = canvas.width;
+    canvas2.height = canvas.height;
+    let contextBuffer = canvas2.getContext('2d');
 
     let size = Math.min(canvas.width, canvas.height)*0.25;    
     let x = -size;
@@ -24,28 +30,32 @@ function init() {
     // robot.renderRobot(context, robot2);
 
     window.requestAnimationFrame((currentTime) => {
-        draw(context, currentTime, currentTime, speed, distanceToTravel, robot1);
+        draw(currentTime, currentTime, speed, distanceToTravel, robot1);
     });    
-}
 
-function draw(context, currentTime, lastTime, speed, distanceToTravel, aRobot) {
-    let deltaTime = currentTime - lastTime;
-    let deltaDistance = speed * deltaTime;
-    
-    let newRobot = aRobot.moveRelative(deltaDistance);
-    gfx.clearCanvas(context);
-    gfx.drawLine(context, 0, aRobot.y+aRobot.size/2, context.canvas.width, aRobot.y+aRobot.size/2, 'white    ');
-    robot.renderRobot(context, newRobot);
-    
-    let newLastTime = currentTime;
-    let newDistanceToTravel = distanceToTravel - deltaDistance;
-    
-    if (newDistanceToTravel <= 0 || speed === 0) {
-        console.log('Finished');
-        return;
+    function draw(currentTime, lastTime, speed, distanceToTravel, aRobot) {
+        let deltaTime = currentTime - lastTime;
+        let deltaDistance = speed * deltaTime;
+        
+        let newRobot = aRobot.moveRelative(deltaDistance);
+        gfx.clearCanvas(contextBuffer);
+        gfx.drawLine(contextBuffer, 0, aRobot.y+aRobot.size/2, context.canvas.width, aRobot.y+aRobot.size/2, 'white');
+        robot.renderRobot(contextBuffer, newRobot);
+        
+        let newLastTime = currentTime;
+        let newDistanceToTravel = distanceToTravel - deltaDistance;
+        
+        if (newDistanceToTravel <= 0 || speed === 0) {
+            // restart
+            console.log('Restarting');
+            distanceToTravel = context.canvas.width + aRobot.size*2 + deltaDistance;
+            newRobot = robot.makeRobot(-newRobot.size, newRobot.y, newRobot.size);
+        }
+        
+        gfx.clearCanvas(context);
+        context.drawImage(canvas2, 0, 0);
+        window.requestAnimationFrame((currentTime) => {
+            draw(currentTime, newLastTime, speed, distanceToTravel-deltaDistance, newRobot);
+        });    
     }
-    
-    window.requestAnimationFrame((currentTime) => {
-        draw(context, currentTime, newLastTime, speed, distanceToTravel-deltaDistance, newRobot);
-    });    
 }
